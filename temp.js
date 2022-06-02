4211cfe76f8f7f9dc3284b1911760853f64b9142
nemmy <ieee1337@gmail.com>
2022-06-02 22:05:12 +0200

    published a functional branch

--- temp.js
+++
@@ -1,8 +0,0 @@
-const arrr = [
-    { "peasant": true, "dinner": "yes", "sex": "hell yeah" },
-    { "peasant": false, "dinner": "no", "sex": "not really" },
-    { "peasant": false, "dinner": "yeah", "sex": "female" }
-];
-
-const res = arrr.filter(yesh => yesh.sex.match(new RegExp("femalE","i")));
-console.log(undefined);
